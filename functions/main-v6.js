71e1fae5ec27d1ac243b2397dbeb3d19126d537d
BeybladeBattleArena <stepmaniaredditrequests@gmail.com>
2026-08-17 15:20:59 -0400

    Use catalog-backed gacha export

--- functions/main-v6.js
+++ functions/main-v6.js
@@ -3,4 +3,6 @@ const progression = require('./progression-v5');
 const singleplayer = require('./singleplayer-v6');
 const secureGift = require('./gift-v7-secure');
 const socialFix = require('./social-v6-fix');
-module.exports = { ...base, ...progression, ...singleplayer, ...secureGift, ...socialFix };
+const catalogGacha = require('./catalog-gacha-v7');
+
+module.exports = { ...base, ...progression, ...singleplayer, ...secureGift, ...socialFix, ...catalogGacha };
