5e8401c550d8784f38442128df86d60449d65505
BeybladeBattleArena <stepmaniaredditrequests@gmail.com>
2026-09-04 21:32:56 -0400

    feat: load Al Hata fork and bridge ambush chunk

--- online/run-ui-bridge-v21.js
+++ online/run-ui-bridge-v21.js
@@ -8,6 +8,7 @@
     '/online/al-hata-stage1-core-v1.js?v=1',
     '/online/al-hata-stage1-beach-v1.js?v=1',
     '/online/al-hata-stage1-jungle-v1.js?v=1',
+    '/online/al-hata-stage1-fork-v1.js?v=1',
   ];
   const WORLD_INSERTION_LINE="      platformSource=requiredReplace(platformSource,renderMarker,worldInjection,'world renderer insertion');";
 
